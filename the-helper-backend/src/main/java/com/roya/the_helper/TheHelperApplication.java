0c5695469c1e3b6138862879f355ff95599b91b0
Aarish <aarishm767@gmail.com>
2025-07-23 14:31:46 +0530

    Initial commit

---
+++ the-helper-backend/src/main/java/com/roya/the_helper/TheHelperApplication.java
@@ -0,0 +1,13 @@
+package com.roya.the_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHelperApplication.class, args);
+	}
+
+}
---
+++ the-helper-backend/src/test/java/com/roya/the_helper/TheHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roya.the_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
