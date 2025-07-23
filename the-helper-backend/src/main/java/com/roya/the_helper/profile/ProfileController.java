4ed7cd3bb74251dca0ae922b1d3369340dc0bd48
Aarish <aarishm767@gmail.com>
2025-07-23 15:57:07 +0530

    prototype-feature: mvp apis added

---
+++ the-helper-backend/src/main/java/com/roya/the_helper/profile/ProfileController.java
@@ -0,0 +1,42 @@
+package com.roya.the_helper.profile;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/profile")
+@RequiredArgsConstructor
+public class ProfileController {
+    private final ProfileService profileService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ProfileEntity> registerProfile(@RequestBody ProfileEntity profile) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(profileService.createProfile(profile));
+    }
+
+    @GetMapping("/{profileId}")
+    public ResponseEntity<ProfileEntity> getProfile(@PathVariable String profileId) {
+        return ResponseEntity.ok(profileService.getProfile(profileId));
+    }
+
+    @PutMapping("/{profileId}")
+    public ResponseEntity<ProfileEntity> updateProfile(@PathVariable String profileId, @RequestBody ProfileEntity profileData) {
+        return ResponseEntity.ok(profileService.updateProfile(profileId, profileData));
+    }
+
+    @DeleteMapping("/{profileId}")
+    public ResponseEntity<String> deleteProfile(@PathVariable String profileId) {
+        profileService.deleteProfile(profileId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Profile deleted successfully");
+    }
+
+    @GetMapping("/{profileId}")
+    public ResponseEntity<List<ProfileEntity>> searchProfile(@RequestParam Map<Integer, String> filters) {
+        return ResponseEntity.ok(profileService.searchProfile(filters));
+    }
+}
